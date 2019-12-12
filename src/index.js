c70bc1b1b609ea58d5b2af6fd21996243d5a09e4
snegmen <che1376@mail.ru>
2019-12-12 22:18:28 +0700

    Доработка формы регистрации/авторизации

--- src/index.js
+++ src/index.js
@@ -15,13 +15,27 @@ const loginForm = new Popup(
   '#signup-form',
   apiBack.login.bind(apiBack),
   apiBack.getUserName.bind(apiBack),
-  // showError,
+);
+
+const signupForm = new Popup(
+  document.querySelector('#signup-form'),
+  '#login-form',
+  apiBack.login.bind(apiBack),
+  apiBack.getUserName.bind(apiBack),
+);
+
+const regForm = new Popup(
+  document.querySelector('#signup-ok'),
+  '#login-form',
+  apiBack.login.bind(apiBack),
+  apiBack.getUserName.bind(apiBack),
 );
 
 const userMenu = new HeadingMenu(
   loginForm.open.bind(loginForm),
   apiBack.logout.bind(apiBack),
 );
+
 userMenu.init();
 
 window.onresize = () => {
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -19,7 +19,20 @@ const loginForm = new Popup(
   '#signup-form',
   apiBack.login.bind(apiBack),
   apiBack.getUserName.bind(apiBack),
-  // showError,
+);
+
+const signupForm = new Popup(
+  document.querySelector('#signup-form'),
+  '#login-form',
+  apiBack.login.bind(apiBack),
+  apiBack.getUserName.bind(apiBack),
+);
+
+const regForm = new Popup(
+  document.querySelector('#signup-ok'),
+  '#login-form',
+  apiBack.login.bind(apiBack),
+  apiBack.getUserName.bind(apiBack),
 );
 
 const userMenu = new HeadingMenu(
